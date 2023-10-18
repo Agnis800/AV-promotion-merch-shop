88474e7889b4c86e23053e26d196ef47c247626f
Agnis800 <agnisvanags@gmail.com>
2023-10-18 14:00:37 +0300

    Pievienota php un datubāze

---
+++ sql/users.sql
@@ -0,0 +1,9 @@
+/*
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+*/
