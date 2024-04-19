b5514b2242ea3cf80de2c7019e6ba7f82fe04022
Agnis800 <agnisvanags@gmail.com>
2024-04-19 10:36:59 +0300

    Veiktas dizaina izmaiņas "form" lapā.

--- pages/form.php
+++ pages/form.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!--"Montserrat" fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <!-- "Source Serif 4" fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Indie+Flower&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie-edge">
@@ -25,8 +30,10 @@
                  <form action="/action_page.php">
                     <label for="name">Name:</label><br>
                     <input type="text" id="name" name="name"><br>
+
                     <label for="email">E-mail:</label><br>
                     <input type="text" id="email" name="email"><br><br>
+                    
                     <input type="submit" value="Submit">
                   </form> 
             </div>
